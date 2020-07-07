d087db8133af6ea0a4b949166218341cc07828db
Lonnie Blansett <lonnie@limbonia.tech>
2020-07-07 15:49:10 -0500

    Make default processing available for settings to the API

--- lib/Traits/Controller/Api/HasSettings.php
+++ lib/Traits/Controller/Api/HasSettings.php
@@ -9,4 +9,42 @@ trait HasSettings
   protected function apiSettingsConstruct()
   {
   }
+
+  /**
+   * Generate and return the list of settings or settings fields
+   *
+   * @return array
+   * @throws \Limbonia\Exception
+   */
+  protected function processApiGetSettings()
+  {
+    if ($this->oRouter->action != 'settings')
+    {
+      throw new \Limbonia\Exception("Invalid settings action: {$this->oRouter->action}");
+    }
+
+    if ($this->oRouter->subaction == 'fields')
+    {
+      return $this->getSettingsFields();
+    }
+
+    return $this->getSetting($this->oRouter->subaction);
+  }
+
+  /**
+   * Create the API specified model with the API specified data then return the created model
+   *
+   * @return array
+   * @throws \Limbonia\Exception
+   */
+  protected function processApiPostSettings()
+  {
+    foreach ($this->oRouter->data as $sSettingName => $xSettingValue)
+    {
+      $this->setSetting($sSettingName, $xSettingValue);
+    }
+
+    $this->saveSettings();
+    return $this->getSetting();
+  }
 }
